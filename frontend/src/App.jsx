8e2a88284c31ad48d18af9a25a9f3b68561dc5f9
Dharmesh Tanwar <dharmesh230335@keshav.du.ac.in>
2025-01-26 18:24:19 +0530

    initial commit with file structures

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>hello Ganesh and Arjun</h1>
+    <h2>Let's go something good</h2>
+    </>
+  )
+}
+
+export default App
